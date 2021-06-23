ce25746b416dd77d68b08fa37bf474561e9d675a
Dmitry Boreiko <arxiomed321@gmail.com>
2021-06-23 11:07:45 +0200

    Поправил код

--- js/offer.js
+++ js/offer.js
@@ -1,62 +1,57 @@
-import {offers} from './generate.js';
+// import {offers} from './generate.js';
 
+function getType (type) {
+  switch (type) {
+    case 'flat':
+      return 'Квартира';
+    case 'bungalow':
+      return 'Бунгало';
+    case 'house':
+      return 'Дом';
+    case 'palace':
+      return 'Дворец';
+    case 'hotel':
+      return 'Отель';
+  }
+}
 
 const renderCard = (ad) => {
 
   const cardTemplate = document.querySelector('#card').content.querySelector('.popup');
   const cardElement = cardTemplate.cloneNode(true);
-  // const features = [ad.offer.features];
 
 
   const cardElementAvatar = cardElement.querySelector('.popup__avatar');
   cardElementAvatar.src = ad.author.author;
-  if (! ad.author.author) {
-    cardElementAvatar.textContent = 'Аватар отсутствует';
-  }
 
   const cardElementTitle = cardElement.querySelector('.popup__title');
   cardElementTitle.textContent = ad.offer.title;
-  if (! ad.offer.title) {
+  if (!ad.offer.title) {
     cardElementTitle.textContent = 'Описание отсутствует';
   }
 
   const cardElementAddress = cardElement.querySelector('.popup__text--address');
   cardElementAddress.textContent = ad.offer.address;
-  if (! ad.offer.address) {
+  if (!ad.offer.address) {
     cardElementAddress.textContent = 'Адрес отсутствует';
   }
 
   const cardElementType = cardElement.querySelector('.popup__type');
-  function getType () {
-    switch (ad.offer.type) {
-      case 'flat':
-        return 'Квартира';
-      case 'bungalow':
-        return 'Бунгало';
-      case 'house':
-        return 'Дом';
-      case 'palace':
-        return 'Дворец';
-      case 'hotel':
-        return 'Отель';
-    }
-  }
-  getType(offers);
-  cardElementType.textContent = getType();
-  if (! ad.offer.type) {
+  cardElementType.textContent = getType(ad.offer.type);
+  if (!ad.offer.type) {
     cardElementType.textContent = 'Описание отсутствует';
   }
 
   const cardElementPrice = cardElement.querySelector('.popup__text--price');
   cardElementPrice.textContent = `${ad.offer.price} ₽/ночь`;
-  if (! ad.offer.price) {
+  if (!ad.offer.price) {
     cardElementPrice.textContent = 'Описание отсутствует';
   }
 
   const cardElementCapacity = cardElement.querySelector('.popup__text--capacity');
   cardElementCapacity.textContent = `${ad.offer.rooms} комнаты для 
     ${ad.offer.guests} гостей`;
-  if ( !ad.offer.rooms) {
+  if (!ad.offer.rooms) {
     cardElementCapacity.textContent = (`Данные о количестве комнат отсутствуют а количество гостей ${ad.offer.guests}`);
   } else if (!ad.offer.guests) {
     cardElementCapacity.textContent = (`Количество комнат ${ad.offer.rooms} а данные о количестве гостей отсутсвуют`);
@@ -69,7 +64,7 @@ const renderCard = (ad) => {
   const cardElementTime = cardElement.querySelector('.popup__text--time');
   cardElementTime.textContent = `Заезд поcле ${ad.offer.checkin}  
     выезд до ${ad.offer.checkout}`;
-  if ( !ad.offer.checkin) {
+  if (!ad.offer.checkin) {
     cardElementTime.textContent = (`Время заезда отстутствует а время выезда после ${ad.offer.checkout}`);
   } else if (!ad.offer.checkout) {
     cardElementTime.textContent = (`Время заезда ${ad.offer.checkin} а время выезда отсутствует`);
@@ -78,18 +73,8 @@ const renderCard = (ad) => {
     cardElementTime.textContent = ('Описание отсутствует');
   }
 
-  // const cardElementPopupList = cardElement.querySelector('.popup__features');
-  // const modifiers = features.map((feature) => `popup__feature--${feature}`);
-  // cardElementPopupList.querySelectorAll('.popup__feature');
-  // cardElementPopupList.querySelectorAll('.popup__feature').forEach((item) => {
-  //   const modifier = item.classList[1];
-  //   if (! modifiers.includes(modifier)) {
-  //     item.remove();
-  //   }
-  // });
-
   const features = ad.offer.features;
-  const featureListElement = cardElement.querySelector('.popup__features');
+  const cardElementFeaturesList = cardElement.querySelector('.popup__features');
 
   // собираем html-строку, состоящую из нужных тегов
   const featuresHtml = features.map(
@@ -97,20 +82,20 @@ const renderCard = (ad) => {
   ).join(''); // методом .join склеиваем его в одну строку
 
   // заменяем полностью html
-  featureListElement.innerHTML = featuresHtml;
-  if (! ad.offer.features) {
-    featureListElement.textContent = 'Описание отсутствует';
+  cardElementFeaturesList.innerHTML = featuresHtml;
+  if (!ad.offer.features) {
+    cardElementFeaturesList.textContent = 'Описание отсутствует';
   }
 
   const cardElementDescription = cardElement.querySelector('.popup__description');
   cardElementDescription.textContent = ad.offer.description;
-  if (! ad.offer.description) {
+  if (!ad.offer.description) {
     cardElementDescription.textContent = 'Описание отсутствует';
   }
 
   const cardElementPhotos = cardElement.querySelector('.popup__photo');
   cardElementPhotos.src = ad.offer.photo;
-  if (! ad.offer.photo) {
+  if (!ad.offer.photo) {
     cardElementPhotos.textContent = 'Описание отсутствует';
   }
 
